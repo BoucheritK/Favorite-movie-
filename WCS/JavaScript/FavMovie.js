236090a40acc2fab6d7fb3594d566bc116bbccc3
Kevin Boucherit <boucherit.kevin@gmail.com>
2019-03-13 15:24:13 +0100

    created index and js trying to use a pop up showing my favorite movie

---
+++ WCS/JavaScript/FavMovie.js
@@ -0,0 +1,7 @@
+
+
+const movieName = "Apocalypse Now"
+const releaseDate = "1979"
+const director = "Francis Ford Coppola"
+const message = "Hello the best movie ever is : " + movieName + " shot in " + releaseDate + " by " + director + "!!" ; 
+alert(message);
